82bee6a9b26cfc20cc52cba74f21736ea918f95e
Thomas-Amann-IPAustralia <thomas.amann@ipaustralia.gov.au>
2026-02-12 02:40:07 +1100

    Create index.tsx

---
+++ frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import OctaviusEditor from "./OctaviusEditor"
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <OctaviusEditor />
+  </React.StrictMode>
+)
